cd6bf62d9cd224a0d1719e15a8eb4644d75b469d
Piyush <Piyush@DESKTOP-PM3UI2C>
2020-04-13 12:58:00 +0530

    movie-catalog

---
+++ movie-catalog-service/src/main/java/com/model/Ratings.java
@@ -0,0 +1,35 @@
+package com.model;
+
+public class Ratings {
+
+	private String movieId;
+
+	public Ratings() {
+		super();
+	}
+
+	private int rating;
+
+	public String getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public Ratings(String movieId, int rating) {
+		super();
+		this.movieId = movieId;
+		this.rating = rating;
+	}
+
+}
