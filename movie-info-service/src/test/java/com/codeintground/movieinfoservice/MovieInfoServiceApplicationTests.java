5b39a0abd79fb9ddce990ca1f0cd19fd26f4bac2
Piyush <Piyush@DESKTOP-PM3UI2C>
2020-04-13 12:59:28 +0530

    movie-info

---
+++ movie-info-service/src/test/java/com/codeintground/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeintground.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
